10e75255e17f8db6f87a8f79b40ba3af22556acf
zalinahacukaeva8-cpu <zalinahacukaeva8@gmail.com>
2026-06-17 15:40:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+let selectedStars = 0;
+let isAdmin = false;
+
+
+// Выбор звезд
+function rate(number) {
+    selectedStars = number;
+
+    let stars = document.querySelectorAll(".stars span");
+
+    stars.forEach((star, index) => {
+        if (index < number) {
+            star.classList.add("active");
+            star.innerHTML = "★";
+        } else {
+            star.classList.remove("active");
+            star.innerHTML = "☆";
+        }
+    });
+}
+
+
+// Добавление отзыва
+function addReview() {
+
+    let name = document.getElementById("name").value.trim();
+    let text = document.getElementById("reviewText").value.trim();
+
+
+    if (text === "") {
+        alert("Напишите отзыв ❤️");
+        return;
+    }
+
+
+    if (selectedStars === 0) {
+        alert("Выберите оценку ⭐");
+        return;
+    }
+
+
+    if (name === "") {
+        name = "Аноним";
+    }
+
+
+    let review = document.createElement("div");
+
+
+    let deleteButton = "";
+
+
+    if (isAdmin) {
+        deleteButton =
+        '<button onclick="this.parentElement.remove()">🗑 Удалить</button>';
+    }
+
+
+    review.innerHTML = `
+        <h3>${name}</h3>
+        <p style="color:gold; font-size:25px;">
+        ${"★".repeat(selectedStars)}
+        </p>
+        <p>${text}</p>
+        ${deleteButton}
+    `;
+
+
+    document.getElementById("reviewList")
+    .appendChild(review);
+
+
+    document.getElementById("name").value = "";
+    document.getElementById("reviewText").value = "";
+
+
+    selectedStars = 0;
+
+
+    document.querySelectorAll(".stars span")
+    .forEach(star => {
+        star.classList.remove("active");
+        star.innerHTML = "☆";
+    });
+
+}
+
+
+// Вход владельца
+function adminLogin() {
+
+    let password = prompt(
+        "Введите пароль владельца"
+    );
+
+
+    if (password === "madina123") {
+
+        isAdmin = true;
+
+        alert(
+            "Вы вошли как владелец 👑"
+        );
+
+    } else {
+
+        alert(
+            "Неверный пароль ❌"
+        );
+
+    }
+
+}
